8604fdf6344c08e6252f455a03b33a01b1d7cbc8
Gavin Quirk <gavinquirk@gmail.com>
2020-05-29 12:21:23 -0700

    isEmpty, size, print, clear

--- stack.js
+++ stack.js
@@ -27,6 +27,35 @@ class Stack {
     console.log(`Top element is ${this.items[this.count - 1]}`);
     return this.items[this.count - 1];
   }
+
+  // Check if stack is empty
+  isEmpty() {
+    console.log(this.count == 0 ? 'Stack is empty' : 'Stack is not empty');
+    return this.count == 0;
+  }
+
+  // Check size of stack
+  size() {
+    console.log(`${this.count} elements in stack`);
+    return this.count;
+  }
+
+  // Print elements in stack
+  print() {
+    let str = '';
+    for (let i = 0; i < this.count; i++) {
+      str += this.items[i] + ' ';
+    }
+    return str;
+  }
+
+  // Clear stack
+  clear() {
+    this.items = [];
+    this.count = 0;
+    console.log('Stack cleared');
+    return this.items;
+  }
 }
 
 const stack = new Stack();
